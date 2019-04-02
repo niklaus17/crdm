4f3c2ca697a130f0eaae7ad17f4122e98eae2d69
NIK <nicolae-n@mail.ru>
2019-04-02 14:01:14 +0300

    set font

--- blancuri/db_connect.php
+++ blancuri/db_connect.php
@@ -5,6 +5,7 @@ $username = "root";
 $password = "";
 $dbname = "crdm";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+mysqli_set_charset($conn,"utf8");
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
--- blancuri/generate_pdf.php
+++ blancuri/generate_pdf.php
@@ -10,14 +10,14 @@ function Header()
 {
     // Logo
     $this->Image('img/logo_dark.png',10,9,70);
-    $this->SetFont('Arial','B',12);
+    $this->SetFont('Helvetica','I',12);
     // Move to the right
     $this->Cell(92);
     // Title
     $this->Cell(50,6,'Blancuri tiparite de pe: ',0,0,'C');
     $this->SetTextColor(255,102,102);
-    $this->Cell(25,6,$_GET['from_date'],1);
-    $this->Cell(25,6,$_GET['to_date'],1);
+    // $this->Cell(25,6,$_GET['from_date'],1);
+    // $this->Cell(25,6,$_GET['to_date'],1);
     // Line break
     $this->Ln(15);
 }
@@ -35,13 +35,15 @@ function Footer()
 }
 }
 
-$display_heading = array('id'=>'Nr.', 'day'=> 'Data', 'model'=> 'Modelul blancului','section'=> 'Sectia','number'=> 'Cantitate','tip'=> 'Tip','name'=> 'Utilizatorul',);
+
+
+$display_heading = array('id'=>'Nr.', 'day'=> 'Data', 'model'=> 'Modelul blancului','section_id'=> 'Sectia','number'=> 'Cantitate','tip_id'=> 'Tip','name'=> 'Utilizatorul',);
 
 if(isset($_GET['from_date']) && isset($_GET['to_date'])) {
-  $result = mysqli_query($conn, "SELECT id, day, model, section, number, tip, name FROM blancuri WHERE day BETWEEN '" . $_GET["from_date"] . "' AND  '" . $_GET["to_date"] . "'") or die("database error:". mysqli_error($connString));
+  $result = mysqli_query($conn, "SELECT id, day, model, section_id, number, tip_id, name FROM blancuri WHERE day BETWEEN '" . $_GET["from_date"] . "' AND  '" . $_GET["to_date"] . "'") or die("database error:". mysqli_error($connString));
 
 } else {
-  $result = mysqli_query($conn, "SELECT id, day, model, section, number, tip, name FROM blancuri") or die("database error:". mysqli_error($connString));
+  $result = mysqli_query($conn, "SELECT id, day, model, section_id, number, tip_id, name FROM blancuri") or die("database error:". mysqli_error($connString));
 
 }
 
@@ -52,44 +54,62 @@ $pdf = new PDF();
 $pdf->AddPage();
 //foter page
 $pdf->AliasNbPages();
-$pdf->SetFont('Arial','B',10);
+$pdf->SetFont('Arial','',10);
 
 
 
 $pdf->SetFillColor(204,255,204);
 $pdf->Cell(10,8,$display_heading['id'],1, null, 'C', true);
-$pdf->Cell(25,8,$display_heading['day'],1, null, 'C', true);
+$pdf->Cell(20,8,$display_heading['day'],1, null, 'C', true);
 $pdf->Cell(57,8,$display_heading['model'],1, null, 'C', true);
-$pdf->Cell(35,8,$display_heading['section'],1, null, 'C', true);
+$pdf->Cell(37,8,$display_heading['section_id'],1, null, 'C', true);
 $pdf->Cell(20,8,$display_heading['number'],1, null, 'C', true);
-$pdf->Cell(15,8,$display_heading['tip'],1, null, 'C', true);
+$pdf->Cell(18,8,$display_heading['tip_id'],1, null, 'C', true);
 $pdf->Cell(30,8,$display_heading['name'],1, null, 'C', true);
 
 
 foreach($result as $row) {
 $pdf->Ln();
 $pdf->Cell(10,8,$row['id'],1, null, 'C');
-$pdf->Cell(25,8,$row['day'],1, null, 'C');
+$pdf->Cell(20,8,$row['day'],1, null, 'C');
 $pdf->Cell(57,8,$row['model'],1);
-$pdf->Cell(35,8,substr($row['section'], 0, 15) . '...',1);
+
+$section_id = $row['section_id'];
+$section_query = "SELECT  section from sectie where id = " . $section_id;
+$result = mysqli_query($conn, $section_query) or die(mysqli_error($conn));
+$section_name = mysqli_fetch_assoc($result)['section'];
+$pdf->Cell(37,8,iconv('UTF-8', 'ASCII//TRANSLIT', substr($section_name, 0, 15)) . '...',1);
+
 $pdf->Cell(20,8,$row['number'],1, null, 'C');
-$pdf->Cell(15,8,$row['tip'],1, null, 'C');
+
+$type_id =  $row['tip_id'];
+$type_query = "SELECT  format from tipul where id = " . $type_id;
+$result = mysqli_query($conn, $type_query)  or die(mysqli_error($conn));
+$type_name = mysqli_fetch_assoc($result)['format'];
+$pdf->Cell(18,8,$type_name,1, null, 'C');
+
 $pdf->Cell(30,8,$row['name'],1,);
 
 }
-    //
-    // $pdf->Ln(20);
-    // $pdf->Cell(30,6,'Total cantitatea: ',0,'C');
-    // $pdf->SetTextColor(255,102,102);
-    // $pdf->Cell(15,6,$row['number'],1, null, 'C');
-    // $pdf->Cell(15,6,$row['tip'],1, null, 'C');
-    //
-    // $pdf->Ln(10);
-    //
-    // $pdf->Cell(30,6,'Total cantitatea: ',0,'C');
-    // $pdf->SetTextColor(255,102,102);
-    // $pdf->Cell(15,6,$row['number'],1, null, 'C');
-    // $pdf->Cell(15,6,$row['tip'],1, null, 'C');
+
+$pdf->Ln(20);
+$count_query = "SELECT SUM(number) as total, tip_id FROM blancuri GROUP BY tip_id";
+$result = mysqli_query($conn, $count_query)  or die(mysqli_error($conn));
+
+foreach($result as $row) {
+
+  $type_id =  $row['tip_id'];
+  $type_query = "SELECT  format from tipul where id = " . $type_id;
+  $result = mysqli_query($conn, $type_query)  or die(mysqli_error($conn));
+  $type_name = mysqli_fetch_assoc($result)['format'];
+
+  $pdf->Cell(30,6,'Total cantitatea: ',0,'C');
+  $pdf->Cell(15,6, $row['total'],1, null, 'C');
+  $pdf->Cell(20,6,$type_name,1, null, 'C');
+  $pdf->Ln(10);
+
+}
+
 
 $pdf->Output();
 
