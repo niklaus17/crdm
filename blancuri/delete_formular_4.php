d82c66f01925a8608b4d8583fbf5bb4dbe401898
NIK <nicolae-n@mail.ru>
2019-06-05 13:21:04 +0300

    ok

--- blancuri/delete_formular_4.php
+++ blancuri/delete_formular_4.php
@@ -4,14 +4,14 @@ include_once("db_connect.php");
         {
         $sql_query="DELETE FROM formular_4 WHERE id='".$_GET['id']."'";
     		$resultset = mysqli_query($conn, $sql_query) or die("database error:". mysqli_error($conn));
-        $sql_query="SELECT * FROM uploads WHERE formular_id='".$_GET['id']."'";
+        $sql_query="SELECT * FROM uploads4 WHERE formular_id='".$_GET['id']."'";
         $resultset = mysqli_query($conn, $sql_query);
         while($row = mysqli_fetch_array($resultset)) {
             if(file_exists('uploads/' . $row['file'])) {
               unlink('uploads/' . $row['file']);
             }
           }
-          $sql_query="DELETE FROM uploads WHERE formular_id='".$_GET['id']."'";
+          $sql_query="DELETE FROM uploads4 WHERE formular_id='".$_GET['id']."'";
           $resultset = mysqli_query($conn, $sql_query);
       }
     header('Location:formular.php');
